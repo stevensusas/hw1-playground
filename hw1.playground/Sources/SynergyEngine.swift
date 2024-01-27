63de6d67a5dbb00a1482392a792d870cb4934832
Anthony Li <me@anli.dev>
2024-01-27 01:01:36 -0500

    Update doc comments for SynergyEngine

--- hw1.playground/Sources/SynergyEngine.swift
+++ hw1.playground/Sources/SynergyEngine.swift
@@ -2,10 +2,10 @@ import Cocoa
 
 /// Super advanced synergy engine for maximum productivity.
 ///
-/// We use an `enum` since this isn't really a type that can be initialized -
-/// we're just using it as a namespace.
+/// We use an `enum` without any cases since this isn't really
+/// a type that can be initialized - we're just using it as a namespace.
 public enum SynergyEngine {
-    /// 🎶
+    /// Activates the patented synergy engine.
     public static func activate() {
         let url = URL(string: "https://www.youtube.com/watch?v=xvFZjo5PgG0")!
         NSWorkspace.shared.open(url)
