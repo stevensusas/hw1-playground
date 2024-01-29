c02d8dd9fa1547358261201f1e3f20f39a269453
Anthony Li <me@anli.dev>
2024-01-29 10:23:47 -0500

    Rename to playIntroductionMeme()

--- hw1.playground/Sources/IntroductionMeme.swift
+++ hw1.playground/Sources/IntroductionMeme.swift
@@ -1,7 +1,7 @@
 import Cocoa
 
-/// Plays the world's best song!
-public func neverGonnaGiveYouUp() {
+/// Plays an introductory song!
+public func playIntroductionMeme() {
     let url = URL(string: "https://www.youtube.com/watch?v=xvFZjo5PgG0")!
     NSWorkspace.shared.open(url)
 }
