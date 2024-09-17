998492a9e7d53f2e4d95caba3b7c9e128ad7e58f
stevensusas <hanqisu@sas.upenn.edu>
2024-09-17 17:09:22 -0400

    added structs

--- hw1.playground/Contents.swift
+++ hw1.playground/Contents.swift
@@ -2,6 +2,90 @@ import SwiftUI
 
 // TODO: Declare any additional structs, classes, enums, or protocols here!
 
+enum Direction: String {
+    case north = "north"
+    case south = "south"
+    case east = "east"
+    case west = "west"
+}
+
+protocol Location {
+    var description: String { get }
+    var exits: [Direction: String] { get }
+    var person: Person? { get }
+    func describe() -> String
+}
+
+protocol Person {
+    var introduction: String { get }
+    var ending: Bool { get }
+    var endingText: String? { get }
+}
+
+struct Annie: Person {
+    let introduction = "Hi! omg you're so cute. I'm Annie I'm an abg from Oakland. I like to rave, listen to Keshi, and get high. Sick party, right? Do you wanna get outta here?"
+    let ending = true
+    let endingText: String? = "You left the party with Annie, the abg from Oakland... have a good night with her bud!"
+}
+
+struct John: Person {
+    let name = "John"
+    let introduction = "You bring any girls? No? Then why you here? get the f out!"
+    let ending = true
+    let endingText: String? = "You got absolutely destroyed by the Castle brother, John. Everyone around you is looking at you like a loser. You're no longer in the mood for a party anymore. Why even make the effort to party? It's better to focus on the grind anyways. You are going back to your dorm to wash, get comfy, call your parents, and call it a night."
+}
+
+struct Casey: Person {
+    let introduction = "Hi I'm Casey! I'm sorry I have a boyfriend already but you're cute. I feel bad for turning you down but I can tell you where the backdoor is to Castle!"
+    let ending = false
+    let endingText: String? = nil
+}
+
+struct Locust: Location {
+    let description = "You are at Locust Walk. Which party to hit up next?"
+    var exits: [Direction: String] = [.north: "Phi Psi", .east: "Fiji", .west: "Ksig", .south: "Castle"]
+    var person: Person? = nil
+    func describe() -> String {
+        return "\(description)"
+    }
+}
+
+struct PhiPsi: Location {
+    let description = "You are at the Phi Psi house. it's so dead there's not a single soul here."
+    var exits: [Direction: String] = [.south: "Locust"]
+    var person: Person? = nil
+    func describe() -> String {
+        return "\(description)"
+    }
+}
+
+struct Castle: Location {
+    let description = "You are at Castle, a grand building often used for events. A tall and strong doorguard suddenly comes to you."
+    var exits: [Direction: String] = [.north: "Locust"]
+    var person: Person? = John()
+    func describe() -> String {
+        return "\(description)"
+    }
+}
+
+struct Fiji: Location {
+    let description = "You are at the Fiji house, a quieter place known for its close-knit community. You see a short, cute Asian girl with long lashes and fishnets."
+    var exits: [Direction: String] = [.west: "Locust"]
+    var person: Person? = Annie()
+    func describe() -> String {
+        return "\(description)"
+    }
+}
+
+struct Ksig: Location {
+    let description = "You are at Ksig, a large house with a reputation for throwing huge parties. You see a girl-- she's not that cute, but you think you can be good friends with her."
+    var exits: [Direction: String] = [.east: "Locust"]
+    var person: Person? = Casey()
+    func describe() -> String {
+        return "\(description)"
+    }
+}
+
 /// Declare your game's behavior and state in this struct.
 ///
 /// This struct will be re-created when the game resets. All game state should
@@ -10,11 +94,8 @@ struct YourGame: AdventureGame {
     /// Returns a title to be displayed at the top of the game.
     ///
     /// You can generate this dynamically based on your game's state.
-    var title: String {
-        // TODO: Change this title
-        return "Generic Adventure Game"
-    }
-    
+    let title = "It's Friday Night at Penn!"
+    var currLocation: Location = Locust()
     /// Runs at the start of every game.
     ///
     /// Use this function to introduce the game to the player.
@@ -22,8 +103,8 @@ struct YourGame: AdventureGame {
     /// - Parameter context: The object you use to write output and end the game.
     mutating func start(context: AdventureGameContext) {
         // TODO: Remove this and implement logic to start your game!
-        playIntroduction()
-        context.write("Welcome to Generic Adventure Game!")
+        context.write("Welcome to Friday Night at Penn! Have fun at the parties out there.")
+        context.write(currLocation.describe())
     }
     
     /// Runs when the user enters a line of input.
@@ -50,9 +131,30 @@ struct YourGame: AdventureGame {
     ///   - input: The line the user typed.
     ///   - context: The object you use to write output and end the game.
     mutating func handle(input: String, context: AdventureGameContext) {
-        // TODO: Parse the input and implement your game logic!
-        context.write("You decide to \(input). It's not very effective.")
-    }
+            let direction = Direction(rawValue: input.lowercased()) // Try to match the input with a direction
+            if let direction = direction, let newLocationName = currLocation.exits[direction] {
+                // Update the location based on the direction input
+                switch newLocationName {
+                case "Phi Psi":
+                    currLocation = PhiPsi()
+                case "Castle":
+                    currLocation = Castle()
+                case "Fiji":
+                    currLocation = Fiji()
+                case "Ksig":
+                    currLocation = Ksig()
+                case "Locust":
+                    currLocation = Locust()
+                default:
+                    context.write("You can't go that way.")
+                    return
+                }
+                context.write("You head \(direction.rawValue) to \(currLocation.name).")
+                context.write(currLocation.describe())  // Show new location description
+            } else {
+                context.write("You can't go that way.")
+            }
+        }
 }
 
 // Leave this line in - this line sets up the UI you see on the right.
